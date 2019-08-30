387067c8011b89a36007bb9756fe88e5f3a2c5d3
LynnKim <LynnKim@152.23.154.119>
2019-08-30 09:17:57 -0400

    Rough draft

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,40 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+				
+		for (int i = 0; i < num; i++) {
+			String firstname = scan.next();
+			
+			char initial = firstname.charAt(0);
+			
+			String lastname = scan.next();
+			int itemnumber = scan.nextInt();
+			
+			int total = 0;
+			
+			for (int a = 0; a < itemnumber; a++) {
+				
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				int price = scan.nextInt();
+				
+				scan.close();
 
-		// Your code follows here.
+				total = total + quantity * price;
+
+			}	
+			
+			System.out.println(initial + ". " + lastname);
+
+		}
+		
+		
+		
+		
+		
 		
 	}
 }
